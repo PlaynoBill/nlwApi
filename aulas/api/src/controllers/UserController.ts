05030dd2cf2ea65cbb2e5c44ec786cc8cbe75351
Carlos E. Gonçalves <carloseduardogoncalvesjunior@gmail.com>
2021-02-25 23:12:40 -0300

    Aula 3 pela metade

--- aulas/api/src/controllers/UserController.ts
+++ aulas/api/src/controllers/UserController.ts
@@ -1,17 +1,17 @@
-import { Request, Response } from  'express';
-import { getRepository } from 'typeorm';
-import { User } from "../models/User";
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
 
 class UserController {
 
     async create(request: Request, response:  Response){
         const { name, email} = request.body;
         
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
-        })
+        });
         if (userAlreadyExists){
             return response.status(400).json({
                 error: "User already exists!",
@@ -29,4 +29,4 @@ class UserController {
 
 }
 
-export{ UserController };
+export { UserController };
